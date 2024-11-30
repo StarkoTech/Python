4f44b9cb1701ceb5c2903b254a0455e30580af9f
suraj <suraj25mahajan96@gmail.com>
2024-11-30 17:48:17 +0530

    sum_all(*args) funtion is added to calaculate sum of all passed arguments

---
+++ Sum_of_arbitrary_numbers.py
@@ -0,0 +1,6 @@
+#write a funtion sum_all(*args) that calculates the sum of all numbers passed as arguments
+def sum_all(*args):
+    return sum(args)
+
+print(sum_all(1,2,3,4))
+print(sum_all(20,30,49,20,34))
