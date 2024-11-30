ef23cdd7614c3d16b230392234dda23b82367b3c
suraj <suraj25mahajan96@gmail.com>
2024-11-30 18:08:48 +0530

    Arbitrary Keyword Arguments file is creat

---
+++ Keword_Details.py
@@ -0,0 +1,8 @@
+# write a funtion student_details(**kwargs) to accept details about a student (name, age, grade, etc.) and print them in a readable format.
+def student_details(**kwargs):
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+student_details(Name="Suraj", Age=28, Grade="A")
+
+
