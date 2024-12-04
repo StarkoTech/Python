1df67d77f7df70a79b45630b0d5b28a75c9c899b
suraj <suraj25mahajan96@gmail.com>
2024-12-04 20:36:59 +0530

    tuple example

---
+++ Tuple.py
@@ -0,0 +1,9 @@
+tuple = (1, 2, 3, 2, 2, 4)
+tuple1 = (1, 3, 4)
+sumtuple = tuple + tuple1
+
+print(sumtuple)
+print(tuple.count(2))
+print(tuple.index(3))
+print("First two element:", tuple[0:2])
+print("Last three element:", tuple[2:5]) 
