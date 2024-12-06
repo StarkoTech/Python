65f3b0361304cd4fd89fc9734c41d5c28cfe225a
suraj <suraj25mahajan96@gmail.com>
2024-12-06 13:30:00 +0530

    dictionary update exercises

--- Dictionaries.py
+++ Dictionaries.py
@@ -28,3 +28,22 @@ for value in person.values():
 #looping through key-value pairs
 for key, value in person.items():
     print(f"{key}: {value}")
+
+
+#Exercises
+# 1 Create and Access: create a dictionary with details about your favorite movie(e.g., title, year, director).
+# print the value associated with the "director" key
+
+Movie = {"Title": "Pushpa2", "Year": 2024, "Director": "Suraj"}
+print(Movie)
+
+#add a new key-value pair to the movie dicctionary for the "rating."
+#update the "year" key to a new value.
+
+Movie["Rating"] = "Four Star"
+Movie["Year"] = 2025
+print(Movie)           
+
+#loop through the movie dictionary and print each key and value.
+for key, value in Movie.items():
+    print(f"{key}: {value}")
