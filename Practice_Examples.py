f4a8d26d6e2ec6a9e1febe741ae28176d12796d6
suraj <suraj25mahajan96@gmail.com>
2024-12-07 11:59:18 +0530

    Python Practice

--- Practice_Examples.py
+++ Practice_Examples.py
@@ -3,3 +3,13 @@ sum1 = 0
 for sum in range(1, 101):
     sum1 = sum + sum1
 print(sum1)
+
+#Factorial: write a program to find the factorial of a number(e.g., 5! = 5 * 4 * 3 * 2 * 1).
+num = int(input("Enter the Number: "))
+num1 = 1
+for i in range(num+1):
+    if i > 0:
+        num1 = i * num1
+        i += 1
+     
+print(f"The Factorial Of {num} is {num1}")
