7792e819f9562069d738ed158bef2cd61eec2788
suraj <suraj25mahajan96@gmail.com>
2024-12-17 15:36:53 +0530

    Practice Q1

---
+++ Practice_Examples1.py
@@ -0,0 +1,5 @@
+#1 Function: Write a funtion calculate_area() that calculates the area of a rectangle given its length and width.
+def calculate_area(length, width):
+    return length * width
+area = calculate_area(4, 5)
+print(area)
